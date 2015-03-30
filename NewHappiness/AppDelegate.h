4460a0f7aa27f77da8c2eccdc133c33dfecf3bca
Johnny <johnny@JohnnydeMacBook-Pro.local>
2015-03-30 11:09:40 +0800

    Initial Commit

---
+++ NewHappiness/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewHappiness
+//
+//  Created by Johnny on 15/3/30.
+//  Copyright (c) 2015年 Johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewHappiness/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewHappiness
+//
+//  Created by Johnny on 15/3/30.
+//  Copyright (c) 2015年 Johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
